fcd78c1f104746d8f9c5e70437ddf0eb46f7239c
Thomas Boulongne <thomas@spill.net>
2018-05-02 19:40:01 +0200

    Same

--- components/Menu.vue
+++ components/Menu.vue
@@ -108,13 +108,9 @@ export default {
 			width: 100%;
 			height: 100%;
 
-			&:nth-child(2) {
-				transform: translate3d(100%, 0, 50vw) rotateY(-90deg);
-			}
+			--selfAngle: var(--index) * 360  / var(--numberOfItems) + var(--computedDegree)
 
-			&:nth-child(3) {
-				transform: translate3d(0, 0, 100vw) rotateY(-180deg);
-			}
+			transform: rotateY(var(--selfAngle) * 1deg) translate3d(, 0, );
 
 			.imgWrapper {
 				position: absolute;
