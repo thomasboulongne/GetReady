bd9f0a0414ef05353aa6bf9eb524b58dddaf8be7
MOUTIER Kevin <kmoutier@g-a102-33.local>
2018-05-28 18:06:24 +0200

    Organize - Add Vue file

---
+++ pages/organize.vue
@@ -0,0 +1,30 @@
+<template>
+	<section class="container organize-page">
+
+	</section>
+</template>
+
+<script>
+
+export default {
+
+	// async fetch({store, isServer, req, res}) {
+	// 	await getAll(store, isServer, req, res);
+	// },
+
+	data() {
+		return {
+
+		};
+	},
+
+	components: {
+
+	}
+};
+
+</script>
+
+<style lang="scss">
+
+</style>
