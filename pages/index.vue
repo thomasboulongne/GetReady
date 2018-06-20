8efcb3912ef0c7d1762cf7b2b96b36a23de2e5d7
Thomas Boulongne <thomasboulongne@hotmail.com>
2018-06-20 02:43:37 +0200

    Build w/o fetch

--- pages/index.vue
+++ pages/index.vue
@@ -6,33 +6,33 @@
 
 <script>
 import PageComp from '~/components/Page';
-import CookiesServ from 'cookie';
+// import CookiesServ from 'cookie';
 import CookiesClient from 'js-cookie';
 
 export default {
-	fetch({ store, redirect, req }) {
-		let goal;
-		if (process.server && req.headers.cookie) {
-			goal = CookiesServ.parse(req.headers.cookie)['reachyourgoal_goal'];
-		} else {
-			goal = CookiesClient.get('reachyourgoal_goal');
-		}
-		if (goal) {
-			store.dispatch('setGoal', goal);
-		}
-		if (store.getters.goal === null) {
-			return redirect('/intro');
-		}
-	},
-	// created() {
-	// 	const goal = CookiesClient.get('reachyourgoal_goal');
-	// 	if (goal) {
-	// 		this.$store.dispatch('setGoal', goal);
+	// fetch({ store, redirect, req }) {
+	// 	let goal;
+	// 	if (process.server && req.headers.cookie) {
+	// 		goal = CookiesServ.parse(req.headers.cookie)['reachyourgoal_goal'];
+	// 	} else {
+	// 		goal = CookiesClient.get('reachyourgoal_goal');
 	// 	}
-	// 	if (this.$store.getters.goal === null) {
-	// 		this.$router.push('/intro');
+	// 	if (goal) {
+	// 		store.dispatch('setGoal', goal);
+	// 	}
+	// 	if (store.getters.goal === null) {
+	// 		return redirect('/intro');
 	// 	}
 	// },
+	created() {
+		const goal = CookiesClient.get('reachyourgoal_goal');
+		if (goal) {
+			this.$store.dispatch('setGoal', goal);
+		}
+		if (this.$store.getters.goal === null) {
+			this.$router.push('/intro');
+		}
+	},
 	data() {
 		return {
 			page: this.$route.name === 'page'
